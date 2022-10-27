946b5ba9909baccadbd5272d6844d2bcdf79fa87
Shunya929 <shunyagoto192@gmail.com>
2022-10-27 11:08:46 +0900

    [add]gulp導入

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-let gulp         = require( 'gulp' );              // 1
+let gulp = require( 'gulp' );
 let sass         = require( 'gulp-sass' );         // 1
 let autoprefixer = require( 'gulp-autoprefixer' ); // 1
 let plumber      = require( 'gulp-plumber' );      // 1
 let sourcemaps   = require( 'gulp-sourcemaps' );   // 1
 
 // Sass
-gulp.task( 'sass', function(){ // 2
-    gulp.src( './src/assets/sass/**/*.scss' ) // 3
+gulp.task('sass', function(){ // 2
+    return gulp.src( './scss/**/*.scss' ) // 3
         .pipe( plumber() ) // 4
         .pipe( progeny() ) // 4
         .pipe( sourcemaps.init() ) // 5
@@ -19,4 +19,85 @@ gulp.task( 'sass', function(){ // 2
         } ) )
         .pipe( sourcemaps.write() ) // 5
         .pipe( gulp.dest( './css/')) // 8
-} );F
+} );
+
+let changed      = require( 'gulp-changed' );
+let imagemin     = require( 'gulp-imagemin' );
+let imageminJpg  = require( 'imagemin-jpeg-recompress' );
+let imageminPng  = require( 'imagemin-pngquant' );
+let imageminGif  = require( 'imagemin-gifsicle' );
+let svgmin       = require( 'gulp-svgmin' );
+
+gulp.task('imagemin', function() {
+    // jpeg,png,gif
+    return gulp.src( './img/**/*.+(png|jpg|jpeg|gif)' ) // 1
+       .pipe( changed( './img' ) ) // 2
+       .pipe( imagemin( [ // 3
+           imageminPng(),
+           imageminJpg(),
+           imageminGif({
+               interlaced: false,
+               optimizationLevel: 3,
+               colors: 180
+           } )
+       ] ) )
+       .pipe( gulp.dest( './img/' ) );
+   // svg
+   return gulp.src( './img/**/*.+(svg)' ) // 4
+       .pipe( changed( './img' ) )
+       .pipe( svgmin() ) // 5
+       .pipe( gulp.dest( './img/' ) );
+} );
+
+let concat       = require( 'gulp-concat' );
+let jshint       = require( 'gulp-jshint' );
+let rename       = require( 'gulp-rename' );
+let uglify       = require( 'gulp-uglify' );
+
+// concat js file(s)
+gulp.task( 'js.concat', function() {
+    return gulp.src( [
+        './js/main.js' // 1
+    ] )
+        .pipe( plumber() )
+        .pipe( jshint() ) // 2
+        .pipe( jshint.reporter( 'default' ) ) // 2
+        .pipe( concat( 'bundle.js' ) ) // 3
+        .pipe( gulp.dest( './js' ) );
+} );
+
+// compress js file(s)
+gulp.task( 'js.compress', function() {
+    return gulp.src( './js/bundle.js' )
+        .pipe( plumber() )
+        .pipe( uglify() ) // 4
+        .pipe( rename( 'bundle.min.js' ) ) // 5
+        .pipe( gulp.dest( './js' ) );
+} );
+
+let browserSync  = require( 'browser-sync' );
+
+// Browser Sync
+gulp.task('bs', function() {
+    browserSync({
+        server: { // 1
+            baseDir: "./",
+            index: "index.html"
+        }
+    });
+});
+
+// Reload Browser
+gulp.task( 'bs-reload', function() {
+    browserSync.reload(); // 2
+});
+
+//
+// Default task
+//
+gulp.task( 'default', [ 'bs', 'sass', 'js.concat', 'js.compress', 'imagemin' ], function() { // 1
+    gulp.watch("./**/*.html", ['bs-reload']); // 2
+    gulp.watch("./scss/**/*.scss", [ 'sass', 'bs-reload' ]); // 3
+    gulp.watch("./js/*.js", [ 'js.concat', 'js.compress', 'bs-reload' ]); // 4
+    gulp.watch("./img/*", [ 'imagemin', 'bs-reload' ]); // 5
+});
--- js/main.js
+++ js/main.js
@@ -1,51 +1,68 @@
-// /*
-//  * Noel Delgado | @pixelia_me
-//  *
-// */
+jQuery(function($){
 
-// let svgElement = document.querySelector('svg');
-// let maskedElement = document.querySelector('#mask-circle');
-// let circleFeedback = document.querySelector('#circle-shadow');
-// let svgPoint = svgElement.createSVGPoint();
+  // let svgElement = document.querySelector('img');
+  // let maskedElement = document.querySelector('#mask-circle');
+  // let circleFeedback = document.querySelector('#circle-shadow');
+  // let svgPoint = svgElement.createSVGPoint();
 
-// function cursorPoint(e, svg) {
-//     svgPoint.x = e.clientX;
-//     svgPoint.y = e.clientY;
-//     return svgPoint.matrixTransform(svg.getScreenCTM().inverse());
-// }
+  // function cursorPoint(e, svg) {
+  //     svgPoint.x = e.clientX;
+  //     svgPoint.y = e.clientY;
+  //     return svgPoint.matrixTransform(svg.getScreenCTM().inverse());
+  // }
 
-// function update(svgCoords) {
-//     maskedElement.setAttribute('cx', svgCoords.x);
-//     maskedElement.setAttribute('cy', svgCoords.y);
-//     circleFeedback.setAttribute('cx', svgCoords.x);
-//     circleFeedback.setAttribute('cy', svgCoords.y);
-// }
+  // function update(svgCoords) {
+  //     maskedElement.setAttribute('cx', svgCoords.x);
+  //     maskedElement.setAttribute('cy', svgCoords.y);
+  //     circleFeedback.setAttribute('cx', svgCoords.x);
+  //     circleFeedback.setAttribute('cy', svgCoords.y);
+  // }
 
-// window.addEventListener('mousemove', function(e) {
-//   update(cursorPoint(e, svgElement));
-// }, false);
+  // let el = $(".c-pic2");
+  //   el.onmousemove = function(event){
+  //   event.offsetX;
+  // };
 
-// document.addEventListener('touchmove', function(e) {
-//     e.preventDefault();
-//     var touch = e.targetTouches[0];
-//     if (touch) {
-//         update(cursorPoint(touch, svgElement));
-//     }
-// }, false);
+  $(function() {
+    var b = $('.c-pic2');
+    console.log('コンテンツ本体：' + b.height() + '×' + b.width());
+    console.log('内部余白込み：' + b.innerHeight() + '×' + b.innerWidth());
+    console.log('枠線込み：' + b.outerHeight() + '×' + b.outerWidth());
+    console.log('外部余白込み：' + b.outerHeight(true) + '×' + b.outerWidth(true));
+  });
 
-let
-cursor = $(".cursor"),
-cWidth = 20, //カーソルの大きさ
-mouseX = 0, //マウスのX座標
-mouseY = 0; //マウスのY座標
+  let
+  cursor = $(".cursor"),
+  cWidth = 20, //カーソルの大きさ
+  mouseX = 0, //マウスのX座標
+  mouseY = 0; //マウスのY座標
 
-$(document).on('mousemove', function(e) {
-  mouseX = e.pageX;
-  mouseY = e.pageY;
-  cursor.css({
-    //カーソルの真ん中に座標軸が来るよう、
-    //カーソルの大きさの半分を引きます
-    left: mouseX - (cWidth / 2),
-    top: mouseY - (cWidth / 2)
-  })
+  $(document).on('mousemove', function(e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    cursor.css({
+      //カーソルの真ん中に座標軸が来るよう、  
+      //カーソルの大きさの半分を引きます
+      left: mouseX - (cWidth / 2),
+      top: mouseY - (cWidth / 2)
+    })
+  });
+
+  // $(".c-pic2").on("mouseover",function() {
+  //   $(".c-pic2").toggleClass("is-hover");
+  //   console.log("jhgajga");
+  //   });
+
+   const box = $(".c-pic2")
+  $(function () {
+    box.hover(
+    function () {
+      $(this).css('opacity', '1');
+    },
+    function () {
+      $(this).css('opacity', '0');
+    }
+  )
+
+});
 });
